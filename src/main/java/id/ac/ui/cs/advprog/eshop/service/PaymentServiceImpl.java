764dd147f60ff93d498e6f2118f85f8a81d78869
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-03-10 02:59:47 +0700

    [GREEN] Implement addPayment logic for Voucher and Bank Transfer

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -2,12 +2,13 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -16,21 +17,43 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        String status = "SUCCESS";
+
+        if (method.equals("VOUCHER")) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode == null || voucherCode.length() != 16 || !voucherCode.startsWith("ESHOP")) {
+                status = "REJECTED";
+            } else {
+                long digitCount = voucherCode.chars().filter(Character::isDigit).count();
+                if (digitCount != 8) {
+                    status = "REJECTED";
+                }
+            }
+        } else if (method.equals("BANK_TRANSFER")) {
+            String bankName = paymentData.get("bankName");
+            String referenceCode = paymentData.get("referenceCode");
+            if (bankName == null || bankName.isBlank() || referenceCode == null || referenceCode.isBlank()) {
+                status = "REJECTED";
+            }
+        }
+
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, status, paymentData);
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        payment = new Payment(payment.getId(), payment.getMethod(), status, payment.getPaymentData());
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
